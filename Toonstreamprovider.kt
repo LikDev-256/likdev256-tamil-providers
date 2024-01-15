1e3fcf98bd1087cf46faa11e5af251328d3fdf6c
Franklin Prakash <77559104+franklinprakash1090@users.noreply.github.com>
2024-01-15 10:24:30 +0530

    Toonstreamprovider.kt

--- Toonstreamprovider.kt
+++ Toonstreamprovider.kt
@@ -8,6 +8,6 @@ import android.content.Context
 class toonstreamProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(BanglaPlexProvider())
+        registerMainAPI(toonstreamProvider())
     }
 }
